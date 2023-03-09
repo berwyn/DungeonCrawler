421fe957fe4de1cf3c1c425d42c40ee4db8cec4f
Berwyn Jamesson <berwyn.codeweaver@gmail.com>
2023-03-09 02:19:29 -0500

    Take a stab at custom event effects

---
+++ Scenes/CellTesting/RevealDoorEffect.cs
@@ -0,0 +1,16 @@
+using DungeonFramework.Nodes;
+
+public partial class RevealDoorEffect : DungeonEventEffect
+{
+    private DungeonCell _cell;
+
+    public override void _Ready()
+    {
+        _cell = GetNode<DungeonCell>("%DungeonCell6");
+    }
+
+    public override void OnActivated()
+    {
+        _cell.EnabledFaces &= ~DungeonCell.Face.West;
+    }
+}
